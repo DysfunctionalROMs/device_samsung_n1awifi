92804aee4dc647d8a2f1f607e27c2feee0c5d651
mosimchah <mosimchah@gmail.com>
2016-12-01 17:51:23 -0500

    Break it

--- broken.mk
+++ broken.mk
@@ -4,10 +4,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from n1awifi device
 $(call inherit-product, device/samsung/n1awifi/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common broken stuff.
+$(call inherit-product, vendor/broken/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_n1awifi
+PRODUCT_NAME := broken_n1awifi
 PRODUCT_DEVICE := n1awifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -15,3 +15,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=n1awifi \
     PRODUCT_DEVICE=n1awifi \
     TARGET_DEVICE=n1awifi
+
+# Broken Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Josh Wiley (Orion116)"
